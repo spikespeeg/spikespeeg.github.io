b877b5e101547e342ee5a174888697c7f4ed84e7
spikespeeg <w19826412651@outlook.com>
2024-08-19 14:57:41 +0800

    Site updated: 2024-08-19 14:57:40

--- js/funny.js
+++ js/funny.js
@@ -2,12 +2,12 @@ var OriginTitle = document.title;
 var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        $('[rel="icon"]').attr('href', "/funny.ico");
+        $('[rel="icon"]').attr('href', "C:\Users\御风\OneDrive\文档\blogs\themes\matery\source\js\funny.ico");
         document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
         clearTimeout(titleTime);
     }
     else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
+        $('[rel="icon"]').attr('href', "/favicon.png");
         document.title = '(ฅ>ω<*ฅ) 噫又好啦 ~' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
